cfe6c77bd45261a51aa0baec87ff0c52670a9f71
meet <meetunpatel.0510@gmail.com>
2023-11-20 23:22:22 +0530

    one-way comments

--- src/pages/query/Query.js
+++ src/pages/query/Query.js
@@ -367,7 +367,7 @@ export default function Query() {
                         ))}
                     </ul>
 
-                    {user.displayName !== 'student' && (
+                    {user.photoURL !== 'student' && (
                       <form
                         className="add-comment"
                         onSubmit={handleSubmitComment}
